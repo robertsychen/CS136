745c82f800cefcead369d3914d92ad789d9b8522
Raynor Kuang <raynorkuang@gmail.com>
2016-04-25 22:49:04 -0400

    add initial project folder

---
+++ dm_project/iter_da.py
@@ -0,0 +1,10 @@
+# here's a boilerplate algorithm for the iterated deferred acceptance
+
+# generate an array of users
+def gen_users(n):
+
+# calculate the sorted preferences for every user
+def calc_prefs(users):
+
+# return matches
+def run_da
---
+++ dm_project/user.py
@@ -0,0 +1,8 @@
+class User(object):
+  def __init__(self, id, features, gender, seeking, prefs):
+    self.id = id
+    self.features = features
+    self.gender = gender
+    self.seeking = seeking
+    self.prefs = prefs
+
